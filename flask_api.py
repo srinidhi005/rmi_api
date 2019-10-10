d5c343425a8ff0306a0fbfa114a81d80817b81d8
root <root@extraction-engine-web-ui.us-central1-a.c.rmi-insights.internal>
2019-10-10 16:29:35 +0000

    flask api

---
+++ flask_api.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask_cors import CORS
+import json
+
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+from db_connection import db_connect
+app = Flask(__name__)
+CORS(app)
+auth = HTTPBasicAuth()
+users = {
+    "rmi_user": generate_password_hash("rmi321!@#"),
+    "test_user": generate_password_hash("test321#")
+}
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users:
+        return check_password_hash(users.get(username), password)
+    return False
+
+@app.route('/actuals')
+@auth.login_required
+def actuals_api():
+    con = db_connect() #connct to database
+    if con is not None:
+        cursor = con.cursor()
+        query = "select * from company_actuals"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        field_names = [i[0] for i in cursor.description]
+        json_string = json.dumps([{description: value for description, value in zip(field_names, row)} for row in rows],sort_keys=True, default=str)
+        con.close() #close database connection
+        return  json_string
+    else:
+        return '{"Error":"DB Connection Error"}'
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=8000,debug=True)
